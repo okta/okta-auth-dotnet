ba4a72c9440cd1a144b56e3e04ae6b651598a62a
Andrii Zhegurov <68990515+andriizhegurov-okta@users.noreply.github.com>
2020-12-23 20:49:54 +0200

    Update Okta.Auth.Sdk/Models/VerifyWebAuthnFactorRequest.cs
    Co-authored-by: Bryan Apellanes <63638027+bryanapellanes-okta@users.noreply.github.com>

--- Okta.Auth.Sdk/Models/VerifyWebAuthnFactorRequest.cs
+++ Okta.Auth.Sdk/Models/VerifyWebAuthnFactorRequest.cs
@@ -1,4 +1,5 @@
-﻿    // <copyright file="VerifyU2fFactorRequest.cs" company="Okta, Inc">
+    // <copyright file="VerifyWebAuthnFactorRequest.cs" company="Okta, Inc">
+
     // Copyright (c) 2018 - present Okta, Inc. All rights reserved.
     // Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
     // </copyright>
